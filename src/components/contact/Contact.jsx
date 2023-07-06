641b1402957183bcc64c6a0b9fc31a4ffe601cf4
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-07-06 16:45:57 -0400

    about

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './contact.css';
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contacts">Contact</section>
   )
 }
 
