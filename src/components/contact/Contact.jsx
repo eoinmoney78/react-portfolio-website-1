f0d1ae4cc408f7affee2f1c4eb489ed9c2dea4a6
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-07-27 19:40:56 -0400

    footer contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,29 +3,42 @@ import './contact.css';
 import { MdEmail } from 'react-icons/md';
 import { FaFacebookMessenger } from 'react-icons/fa';
 import {BsWhatsapp } from 'react-icons/bs';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_b95380b', 'template_ip77w5d', form.current, 'iWCrP9nhR0iqCjX60')
+    e.target.reset();
+   
+  };
+
   return (
-    <section id="contacts">
+    <section id="contact">
       <h5>Get In touch</h5>
       <h2>Contact Me</h2>
       <div className="container contact-container">
         <div className="contact-options">
-        <article className="contact-options">
-          <MdEmail/>
+        <article className="contact-option">
+          <MdEmail className="contact-option-icon"/>
           <h4>Email</h4>
           <h5>eoinnoonan8@gmail.com</h5>
           <a href="mailto:eoinnoonan@gmail.com" target="_blank">Send A Message</a>
         </article>
 
-        <article className="contact-options">
-          <FaFacebookMessenger/>
+        <article className="contact-option">
+          <FaFacebookMessenger className="contact-option-icon"/>
           <h4>Messanger</h4>
           <h5> Facebook messanger</h5>
           <a href="https://m.me/eoin.noonan.73" target="_blank">Send A Message</a>
         </article>
 
-        <article className="contact-options">
-      <BsWhatsapp/>
+        <article className="contact-option">
+      <BsWhatsapp className="contact-option-icon"/>
       <h4>Whats App</h4>
       <h5>18028554341</h5> 
       <a href="https://api.whatsapp.com/send?phone=18028554341" target="_blank">Send A Message</a>
@@ -34,7 +47,12 @@ const Contact = () => {
 
         </div>
         {/* End Of Contact Options */}
-        <form action="eoinyakyak@gmail.com"></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name'/>
+          <input type="email" name="email" placeholder="Your Email"  required />
+          <textarea name="message" rows="7" placeholder="Your Message" required/>
+          <button type="submit" className="btn btn-primary">Send</button>
+        </form>
       </div>
     </section>
   )
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import './footer.css';
+import { BsFacebook } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
+import {BsTwitter} from 'react-icons/bs';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer-logo">NOONAN</a>
+      <ul className="permalinks">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+
+      </ul>
+      <div className="footer-socials">
+
+        <a href="https://facebook.com"><BsFacebook/> </a>
+   
+        <a href="https://instagram.com"><BsInstagram/></a>
+
+        <a href="https://twitter.com"><BsTwitter/></a>
+      </div>
+      <div className="footer-copyright">
+        <small>&copy; Noonan web development. All rights Reserved.</small>
+      </div>
+    </footer>
   )
 }
 
