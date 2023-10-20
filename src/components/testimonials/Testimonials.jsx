6a88f16b66e92bab686dc5d6e7e784be220106cf
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-10-20 11:44:54 -0400

    pic update

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './testimonials.css';
-import AVTR1 from '../../assets/avatar1.jpg';
+import AVTR1 from '../../assets/jer-pic.PNG';
 // import AVTR2 from '../../assets/avatar2.jpg';
 // import AVTR3 from '../../assets/avatar3.jpg';
 // import AVTR4 from '../../assets/avatar4.jpg';
